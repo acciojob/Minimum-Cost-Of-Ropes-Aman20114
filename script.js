42e2d8d5d48fa00680a9c0348dcd1872dc958600
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-02-24 16:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your code here
+function minCost(event) {
+
+	event.preventDefault();
+	var inputElement = document.querySelector('input').value;
+	var arr = inputElement.split(',');
+
+	arr.sort(function (a , b) {return a - b});
+
+	var cost = 0;
+	var res =0;
+	while(arr.length > 1){
+		var res = Number(arr[0]) + Number(arr[1]);
+		
+		
+		
+
+		arr.shift();
+		arr.shift();
+		arr.push(res);
+		cost += res;
+		
+		
+		arr.sort(function (a , b) {return a -b});
+	}
+	document.getElementById("result").textContent = cost;
+}
